8feb52226794461c9df664945be134f14b2ad52f
CastroFabio <fmpc95@gmail.com>
2023-12-15 18:54:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <>
+      <section>
+        <button>Historico</button>
+        <button>Comandas</button>
+        <button>Cozinha</button>
+        <button>Cadastro de Produtos</button>
+        <button>Cadastro de Clientes</button>
+      </section>
+      <section>
+        <button>Kaborge</button>
+        <button>Chico</button>
+        <button>Neguinho</button>
+        <button>Bolores</button>
+        <button>Careca</button>
+      </section>
+      <section>
+        <ul>
+          <li>Produto 1: ## reais</li>
+          <li>Produto 2: ## reais</li>
+          <li>Produto 3: ## reais</li>
+        </ul>
+      </section>
+      <section>
+        <button>Comanda-Kaborge Lanche: Sanduíche Finalizar</button>
+        <button>Comanda-Kaborge Lanche: Sanduíche Finalizar</button>
+        <button>Comanda-Kaborge Lanche: Sanduíche Finalizar</button>
+        <button>Comanda-Kaborge Lanche: Sanduíche Finalizar</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
